4974da52ac97aec7e97ada7d7954b126eab6eab4
kutimiti1234 <tarbit.offensive@gmail.com>
2024-08-31 17:49:43 +0900

    clean:Replace forloop to be more readably

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,9 +3,10 @@ import minimist from "minimist";
 import { DateTime, Interval } from "luxon";
 
 const argv = minimist(process.argv.slice(2));
-const year = argv["y"] || dt.year;
-const month = argv["m"] || dt.month;
-const dt = DateTime.local(year, month);
+const now = DateTime.now();
+const year = argv["y"] || now.year;
+const month = argv["m"] || now.month;
+const dt = now || DateTime.local(year, month);
 
 const first_date = dt.startOf("month");
 const last_date = dt.endOf("month");
@@ -20,19 +21,18 @@ const renderBody = function (first_date, last_date) {
   const intervals = Interval.fromDateTimes(first_date, last_date)
     .splitBy({ day: 1 })
     .map((d) => d.start);
+  const blanks = Array(first_date.weekday % 7).fill("   ");
 
   let body = [];
 
-  for (let i = 1; i <= first_date.weekday % 7; i++) {
-    body[i - 1] = " ".padStart(2) + " ";
-  }
-
-  for (const date of intervals) {
+  body = body.concat(blanks);
+  intervals.forEach((date) => {
     body.push(date.day.toString().padStart(2) + " ");
     if (date.weekday === 6) {
       body.push("\n");
     }
-  }
+  });
+
   return body.join("");
 };
 
