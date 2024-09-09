687ebc385bad541e3c4bce4df21e472db64f9588
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-09 23:47:07 +0900

    add:add input validation for both month and year variables

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,6 +3,26 @@
 import minimist from "minimist";
 import * as luxon from "luxon";
 
+const checkYear = function (year) {
+  if (typeof year !== "number") {
+    throw new TypeError(`cal: not a valid year ${year}`);
+  } else if (year < 1 || year > 9999) {
+    throw new RangeError(`cal: year \`${year}\`not in range 1..9999`);
+  }
+};
+
+const checkMonth = function (month) {
+  if (typeof month !== "number") {
+    throw new TypeError(
+      `cal: ${month} is neither a month number (1..12) nor a name.`,
+    );
+  } else if (month < 1 || month > 12) {
+    throw new RangeError(
+      `cal: ${month} is neither a month number (1..12) nor a name.`,
+    );
+  }
+};
+
 const renderHeader = function (month, year) {
   const header = `      ${month}月 ${year}\n日 月 火 水 木 金 土`;
   return header;
@@ -29,10 +49,23 @@ const renderBody = function (firstDate, lastDate) {
   return body;
 };
 
-const argv = minimist(process.argv.slice(2));
 const now = luxon.DateTime.now();
-const year = argv.y ? argv.y : now.year;
-const month = argv.m ? argv.m : now.month;
+const defaultOption = {
+  default: {
+    y: now.year,
+    m: now.month,
+  },
+};
+const argv = minimist(process.argv.slice(2), defaultOption);
+const year = argv.y;
+const month = argv.m;
+try {
+  checkYear(year);
+  checkMonth(month);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
 
 const header = renderHeader(month, year);
 const firstDate = luxon.DateTime.local(year, month, 1);
