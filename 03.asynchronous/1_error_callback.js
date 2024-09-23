14d92ce631d07b9c5acf71f4b833ea7b270af56f
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-23 15:33:42 +0900

    add:the program that triggers erros intentionally using callback functions

---
+++ 03.asynchronous/1_error_callback.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import sqlite3 from "sqlite3";
+
+const database = new sqlite3.Database(":memory:");
+database.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title UNIQUE NOT NULL)",
+  () =>
+    database.run(
+      "INSERT INTO books(title) VALUES($title) ",
+      {
+        $title: null,
+      },
+      function (error) {
+        if (error) {
+          console.log(error.message);
+        }
+        database.get(
+          "SELECT error FROM books WHERE id = $id",
+          {
+            $id: 1,
+          },
+          (error) => {
+            if (error) {
+              console.log(error.message);
+            }
+            database.run("DROP TABLE books", () => database.close);
+          },
+        );
+      },
+    ),
+);
