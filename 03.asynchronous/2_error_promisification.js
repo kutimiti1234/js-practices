a1ce8c046ab34f0014a4f0e5cd70655d1ea99439
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-23 15:37:46 +0900

    add:the program that triggers erros intentionally using promisified functions

---
+++ 03.asynchronous/2_error_promisification.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import mysqlite3 from "./promisified_functions.js";
+
+const database = new sqlite3.Database(":memory:");
+
+mysqlite3
+  .promisifiedRun(
+    database,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title UNIQUE NOT NULL)",
+  )
+  .then(() =>
+    mysqlite3.promisifiedRun(
+      database,
+      "INSERT INTO books(title) VALUES($title) ",
+      {
+        $title: null,
+      },
+    ),
+  )
+  .catch((error) => console.log(error.message))
+  .then(() =>
+    mysqlite3.promisifiedGet(
+      database,
+      "SELECT error FROM books WHERE id = $id",
+      {
+        $id: 1,
+      },
+    ),
+  )
+  .catch((error) => console.log(error.message))
+  .then(() => mysqlite3.promisifiedRun(database, "DROP TABLE books"))
+  .then(() => database.close);
