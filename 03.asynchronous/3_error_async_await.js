d56be76980eb3b0aec2283be24a2a5d1b533e857
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-23 15:40:45 +0900

    add:the program that triggers errors intentionally using async/await for promisified functions

---
+++ 03.asynchronous/3_error_async_await.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import mysqlite3 from "./promisified_functions.js";
+
+(async () => {
+  const database = new sqlite3.Database(":memory:");
+  try {
+    await mysqlite3.promisifiedRun(
+      database,
+      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT,title UNIQUE NOT NULL)",
+    );
+    let addedID = await mysqlite3.promisifiedRun(
+      database,
+      "INSERT INTO books(title) VALUES($title) ",
+      { $title: null },
+    );
+    console.log(addedID);
+  } catch (error) {
+    console.log(error.message);
+  }
+  try {
+    let row = await mysqlite3.promisifiedGet(
+      database,
+      "SELECT error FROM books WHERE id = $id",
+      {
+        $id: 1,
+      },
+    );
+    console.log(row.title);
+    await mysqlite3.promisifiedRun(database, "DROP TABLE books");
+  } catch (error) {
+    console.log(error.message);
+  }
+  database.close;
+})();
