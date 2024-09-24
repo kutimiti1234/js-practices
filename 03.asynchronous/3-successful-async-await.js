25f107d3ac6cae40c3d440f978089d40dd63cb44
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-24 21:45:01 +0900

    clean:delete unnecessary try catch

--- 03.asynchronous/3-successful-async-await.js
+++ 03.asynchronous/3-successful-async-await.js
@@ -5,28 +5,24 @@ import promisifiedFunctions from "./promisified-functions.js";
 
 (async () => {
   const database = new sqlite3.Database(":memory:");
-  try {
-    await promisifiedFunctions.run(
-      database,
-      "CREATE TABLE books ( id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NUL )",
-    );
-    const addedID = await promisifiedFunctions.run(
-      database,
-      "INSERT INTO books(title) VALUES($title)",
-      { $title: "test_book" },
-    );
-    console.log(addedID);
-    const row = await promisifiedFunctions.get(
-      database,
-      "SELECT * FROM books WHERE id = $id",
-      {
-        $id: 1,
-      },
-    );
-    console.log(row.title);
-    await promisifiedFunctions.run(database, "DROP TABLE books");
-  } catch (error) {
-    console.log(error.message);
-  }
+  await promisifiedFunctions.run(
+    database,
+    "CREATE TABLE books ( id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NUL )",
+  );
+  const addedID = await promisifiedFunctions.run(
+    database,
+    "INSERT INTO books(title) VALUES($title)",
+    { $title: "test_book" },
+  );
+  console.log(addedID);
+  const row = await promisifiedFunctions.get(
+    database,
+    "SELECT * FROM books WHERE id = $id",
+    {
+      $id: 1,
+    },
+  );
+  console.log(row.title);
+  await promisifiedFunctions.run(database, "DROP TABLE books");
   database.close;
 })();
