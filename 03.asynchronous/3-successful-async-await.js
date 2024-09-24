34b5e9aa6f1ad4b4f3ebd0761a9b105b381d17a4
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-24 22:52:38 +0900

    clean:use Top level await if you can

--- 03.asynchronous/3-successful-async-await.js
+++ 03.asynchronous/3-successful-async-await.js
@@ -3,26 +3,24 @@
 import sqlite3 from "sqlite3";
 import promisifiedFunctions from "./promisified-functions.js";
 
-(async () => {
-  const database = new sqlite3.Database(":memory:");
-  await promisifiedFunctions.run(
-    database,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NULL)",
-  );
-  const thisProperties = await promisifiedFunctions.run(
-    database,
-    "INSERT INTO books(title) VALUES($title)",
-    { $title: "test_book" },
-  );
-  console.log(thisProperties.lastID);
-  const row = await promisifiedFunctions.get(
-    database,
-    "SELECT * FROM books WHERE id = $id",
-    {
-      $id: 1,
-    },
-  );
-  console.log(row.title);
-  await promisifiedFunctions.run(database, "DROP TABLE books");
-  database.close;
-})();
+const database = new sqlite3.Database(":memory:");
+await promisifiedFunctions.run(
+  database,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NULL)",
+);
+const thisProperties = await promisifiedFunctions.run(
+  database,
+  "INSERT INTO books(title) VALUES($title)",
+  { $title: "test_book" },
+);
+console.log(thisProperties.lastID);
+const row = await promisifiedFunctions.get(
+  database,
+  "SELECT * FROM books WHERE id = $id",
+  {
+    $id: 1,
+  },
+);
+console.log(row.title);
+await promisifiedFunctions.run(database, "DROP TABLE books");
+database.close;
--- 03.asynchronous/3-unsuccessful-async-await.js
+++ 03.asynchronous/3-unsuccessful-async-await.js
@@ -3,38 +3,36 @@
 import sqlite3 from "sqlite3";
 import promisifiedFunctions from "./promisified-functions.js";
 
-(async () => {
-  const database = new sqlite3.Database(":memory:");
-  await promisifiedFunctions.run(
+const database = new sqlite3.Database(":memory:");
+await promisifiedFunctions.run(
+  database,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NULL)",
+);
+try {
+  const thisProperties = await promisifiedFunctions.run(
     database,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title UNIQUE NOT NULL)",
+    "INSERT INTO books(title) VALUES($title)",
+    { $title: null },
   );
-  try {
-    const thisProperties = await promisifiedFunctions.run(
-      database,
-      "INSERT INTO books(title) VALUES($title)",
-      { $title: null },
-    );
-    console.log(thisProperties.lastID);
-  } catch (error) {
-    if (error.code === "SQLITE_CONSTRAINT") {
-      console.error(error.message);
-    }
+  console.log(thisProperties.lastID);
+} catch (error) {
+  if (error.code === "SQLITE_CONSTRAINT") {
+    console.error(error.message);
   }
-  try {
-    const row = await promisifiedFunctions.get(
-      database,
-      "SELECT error FROM books WHERE id = $id",
-      {
-        $id: 1,
-      },
-    );
-    console.log(row.title);
-  } catch (error) {
-    if (error.code === "SQLITE_ERROR") {
-      console.error(error.message);
-    }
+}
+try {
+  const row = await promisifiedFunctions.get(
+    database,
+    "SELECT error FROM books WHERE id = $id",
+    {
+      $id: 1,
+    },
+  );
+  console.log(row.title);
+} catch (error) {
+  if (error.code === "SQLITE_ERROR") {
+    console.error(error.message);
   }
-  await promisifiedFunctions.run(database, "DROP TABLE books");
-  database.close;
-})();
+}
+await promisifiedFunctions.run(database, "DROP TABLE books");
+database.close;
