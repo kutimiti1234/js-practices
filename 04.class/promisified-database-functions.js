daf50dab2d9b604613c4472538736dde88841af1
kutimiti1234 <tarbit.offensive@gmail.com>
2024-10-08 22:55:49 +0900

    add:promisifiedDatabaseFunctions

---
+++ 04.class/promisified-database-functions.js
@@ -0,0 +1,36 @@
+function run(database, sql, param) {
+  return new Promise((resolve, reject) => {
+    database.run(sql, param, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+}
+
+function get(database, sql, param) {
+  return new Promise((resolve, reject) => {
+    database.get(sql, param, (error, row) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+}
+
+function close(database) {
+  return new Promise((resolve, reject) => {
+    database.close((error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+export default { run, get, close };
