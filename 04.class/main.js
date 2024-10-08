55dd197aacc6915d84ed54c01ebc4bc41076fd31
kutimiti1234 <tarbit.offensive@gmail.com>
2024-10-08 23:00:37 +0900

    feat:add memo creation feature as per requirements

---
+++ 04.class/main.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import minimist from "minimist";
+import sqlite3 from "sqlite3";
+import readline from "readline";
+import manager from "./manager.js";
+import promisifiedDatabaseFunctions from "./promisified-database-functions.js";
+
+const options = minimist(process.argv.slice(2), {
+  alias: {
+    l: "list",
+    r: "reference",
+    d: "delete",
+  },
+});
+
+const database = new sqlite3.Database(".sqlite3");
+await promisifiedDatabaseFunctions.run(
+  database,
+  "CREATE TABLE IF NOT EXISTS memo(id INTEGER PRIMARY KEY AUTOINCREMENT, title NOT NULL, body NOT NULL)",
+);
+const memoManager = new manager.Manager(database);
+
+if (!options.list && !options.reference && !options.delete) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  let lines = [];
+  rl.on("line", (line) => {
+    lines.push(line);
+  });
+  rl.on("close", async () => {
+    await memoManager.add(lines[0], lines.slice(1).join("\n"));
+  });
+}
---
+++ 04.class/manager.js
@@ -0,0 +1,20 @@
+import promisifiedDatabaseFunctions from "./promisified-database-functions.js";
+class Manager {
+  constructor(database) {
+    this.database = database;
+  }
+
+  async add(title, body) {
+    await promisifiedDatabaseFunctions.run(
+      this.database,
+      "INSERT INTO memo(title,body) values($title,$body)",
+      {
+        $title: title,
+        $body: body,
+      },
+    );
+    await promisifiedDatabaseFunctions.close(this.database);
+  }
+}
+
+export default { Manager };
